ad652dd8d577313956f67c6d9209c4f090207e87
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-11-05 21:51:17 +0200

    1

---
+++ src/components/Head.jsx
@@ -0,0 +1,7 @@
+export const Head = ({ clas, id }) => {
+  return (
+    <p className={clas} id={id}>
+      I am Head
+    </p>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import data from "./data.json";
+// import Card from "./components/Card";
+// import Card from "./components/Card"; // Переконайтеся, що "Card" імпортується як "default"
+import Card from "./components/Card"; // Переконайтеся, що ім'я 'Card' відповідає експорту в 'Card.jsx'
+
 // import "./index.css";
 
-const Card = () => {
-  return data.map((photo) => {
-    return (
-      <div
-        key={photo.id}
-        className="card mx-auto my-2"
-        style={{ width: "18rem" }}
-      >
-        <img src={photo.url} className="card-img-top" alt={photo.title} />
-        <div className="card-body">
-          <h5 className="card-title">Card title: {photo.title}</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
-    );
-  });
-};
 // console.log(card);
 ReactDOM.createRoot(document.getElementById("root")).render(<Card />);
 
